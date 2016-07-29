997967709b7110432a6c79793e7b0315b1141fd1
Hannah_Brannan <hannahbrannan@gmail.com>
2016-07-29 13:20:50 -0700

    mod server

--- server/s3.js
+++ server/s3.js
@@ -31,7 +31,7 @@ const postTheVideo = (videoFile) => {
         else {
           process.env.video_id += 1;
           console.log('vidId', process.env.video_id);
-          console.log("Successfully uploaded data to myBucket");
+          console.log("Successfully uploaded video to myBucket");
           } 
 
      });
@@ -52,8 +52,6 @@ const postTheVideo = (videoFile) => {
 
 const postThePhoto = (photo) => {
   console.log('Attempting post of videoFile to s3')
- s3.createBucket({Bucket: process.env.bucket}, function() {
-  //THE BODY IS WHAT YOUR ARE INPUTTING, the KEY IS THE TITLE!
   var params = {
     Bucket: process.env.bucket,
     Key: `Photo for Kairos ${process.env.photo_id}`,
@@ -61,22 +59,30 @@ const postThePhoto = (photo) => {
     ContentType: 'image/jpeg',
     'Content-Transfer-Encoding': base64
   };
+ s3.createBucket({Bucket: process.env.bucket}, function() {
+  //THE BODY IS WHAT YOUR ARE INPUTTING, the KEY IS THE TITLE!
 
   s3.putObject(params, function(err, data) {
       if (err) {    
           console.log(err) 
       }    
       else {
-        console.log("Successfully uploaded data to myBucket" + data);
+        console.log("Successfully uploaded photo to myBucket" + data);
         process.env.photo_id += 1;
         return data; 
       }
   });
 
 });
+    var publicUrl = 'https://s3-us-west-1.amazonaws.com/' + params.Bucket + '/' + params.Key;
+    var presignedUrl = s3.getSignedUrl('putObject', params);
+
+    //send them off
+    return {publicUrl:publicUrl, presignedUrl: presignedUrl};
 };
 
 module.exports.postTheVideo = postTheVideo;
+module.exports.postThePhoto = postThePhoto;
 
 
 
